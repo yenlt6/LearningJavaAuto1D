44f3add4e1cbb730fdc58e55cc650f28f61efce4
Yen Le <yenlt6@onemount.com>
2021-06-01 18:50:54 +0700

    yen

---
+++ src/test/java/oneD/buoi12/AssertExampleTest.java
@@ -0,0 +1,53 @@
+package oneD.buoi12;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.AbstractStringAssert;
+import org.assertj.core.api.Java6Assertions;
+import org.assertj.core.data.Offset;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+    public class AssertExampleTest {
+
+        @Test
+        public void testMethodInt() {
+            int[] methodInt = new int[]{1, 2, 3, 4, 5, 6};
+            Java6Assertions.assertThat(methodInt).contains(new int[]{6}).isNotEmpty().hasSize(6).containsSequence(new int[]{1, 2});
+        }
+
+        @Test
+        public void testString() {
+            String say = "Chị không muốn nhiều bug nhưng bug nhiều nên chị phải fix";
+            ((AbstractStringAssert)((AbstractStringAssert)((AbstractStringAssert)((AbstractStringAssert)Java6Assertions.assertThat(say).isNotNull()).startsWith("Chị")).doesNotContain(new CharSequence[]{"Anh"})).endsWith("fix")).contains(new CharSequence[]{"bug"});
+            Java6Assertions.assertThat(say).isEqualTo(say);
+        }
+
+        @Test
+        public void testNumber() {
+            Double value = 12.0D;
+            Double value1 = 10.0D;
+            Java6Assertions.assertThat(value).isEqualTo(12.0D);
+            Java6Assertions.assertThat(value).isCloseTo(15.0D, Offset.offset(4.0D));
+            Java6Assertions.assertThat(value1).isStrictlyBetween(9.0D, 15.0D);
+            Java6Assertions.assertThat(value).isBetween(10.0D, 15.0D);
+        }
+
+        @Test
+        @DisplayName("Test Case phone number")
+        public void testPhoneNumber() {
+            String phoneNumber = "0919348512";
+            ((AbstractStringAssert)((AbstractStringAssert)Java6Assertions.assertThat(phoneNumber).startsWith("0")).hasSize(10)).containsOnlyDigits();
+        }
+
+        @Test
+        public void testEmail() {
+            String email = "yenlt6@onemount.com";
+            ((AbstractStringAssert)((AbstractStringAssert)Java6Assertions.assertThat(email).contains(new CharSequence[]{"@"})).doesNotContain(new CharSequence[]{"#$%"})).contains(new CharSequence[]{"."});
+        }
+
+        @Test
+        public void testEmail2() {
+            String email = "hien@onemount.com";
+            Java6Assertions.assertThat(email).containsPattern("^[a-zA-Z][\\w-]+@([\\w]+\\.[\\w]+|[\\w]+\\.[\\w]{2,}\\.[\\w]{2,})$");
+        }
+    }
